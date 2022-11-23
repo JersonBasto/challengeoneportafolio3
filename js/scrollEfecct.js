24e467f7a1a2994e1150cd7bfd7c887268ee70b9
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2022-11-23 14:07:45 -0500

    css

---
+++ js/scrollEfecct.js
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+const sections = document.querySelectorAll(".translate");
+const proyectoItem = document.querySelectorAll(".proyecto_item");
+const formacionItem = document.querySelectorAll(".formacion_item");
+const hobbiesItem = document.querySelectorAll(".hobbies_item");
+
+const skillsItem = document.querySelectorAll(".skills_item");
+
+const observar = new IntersectionObserver(
+  (entradas) => {
+    entradas.forEach((entrada) => {
+      entrada.target.classList.toggle("show", entrada.isIntersecting);
+      if (entrada.isIntersecting) observar.unobserve(entrada.target);
+    });
+  },
+  {
+    rootMargin: "-50px",
+  }
+);
+
+sections.forEach((section) => {
+  observar.observe(section);
+});
+
+proyectoItem.forEach((item) => {
+  observar.observe(item);
+});
+formacionItem.forEach((item) => {
+  observar.observe(item);
+});
+hobbiesItem.forEach((item) => {
+  observar.observe(item);
+});
+skillsItem.forEach((item) => {
+  observar.observe(item);
+});
+/**
+ *
+ */
