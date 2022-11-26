f832ba6a3dab3f3a739cac71d478afe461354604
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2022-11-25 20:30:05 -0500

    imagenes

--- js/clickButton.js
+++ js/clickButton.js
@@ -1,3 +1,4 @@
+
 const encriptador = () => {
   console.log("Ir");
   Swal.fire({
@@ -5,8 +6,70 @@ const encriptador = () => {
     icon: "question",
     html:
       '<div class="swal_container">' +
-      '<a class="a_swal" href="https://github.com/JersonBasto/challengeonejuegoahorcado3" target="_blank">Git-Hub</a>' +
+      '<a class="a_swal" href="https://github.com/JersonBasto/challengeonecodificador3" target="_blank">Git-Hub</a>' +
       '<a class="a_swal" href="https://jersonbasto.github.io/challengeonecodificador3/" target="_blank">Pagina Web</a>' +
+      '</div>',
+  });
+};
+
+const ahorcado = () => {
+  console.log("Ir");
+  Swal.fire({
+    title: "¿ A donde quieres ir?",
+    icon: "question",
+    html:
+      '<div class="swal_container">' +
+      '<a class="a_swal" href="https://github.com/JersonBasto/challengeonejuegoahorcado3" target="_blank">Git-Hub</a>' +
+      '<a class="a_swal" href="https://jersonbasto.github.io/challengeonejuegoahorcado3/" target="_blank">Pagina Web</a>' +
+      '</div>',
+  });
+};
+
+const juegoDados = () => {
+  console.log("Ir");
+  Swal.fire({
+    title: "¿ A donde quieres ir?",
+    icon: "question",
+    html:
+      '<div class="swal_container">' +
+      '<a class="a_swal" href="https://github.com/JersonBasto/Talleres/tree/main/Taller6" target="_blank">Git-Hub</a>' +
+      '</div>',
+  });
+};
+
+const libretaCelular = () => {
+  console.log("Ir");
+  Swal.fire({
+    title: "¿ A donde quieres ir?",
+    icon: "question",
+    html:
+      '<div class="swal_container">' +
+      '<a class="a_swal" href="https://github.com/JersonBasto/TallerSpring" target="_blank">Git-Hub</a>' +
+      '</div>',
+  });
+};
+
+const Bingo = () => {
+  console.log("Ir");
+  Swal.fire({
+    title: "¿ A donde quieres ir?",
+    icon: "question",
+    html:
+      '<div class="swal_container">' +
+      '<a class="a_swal" href="https://github.com/JersonBasto/TallerFinalBingo" target="_blank">Git-Hub</a>' +
+      '</div>',
+  });
+};
+
+const EducaTeam = () => {
+  console.log("Ir");
+  Swal.fire({
+    title: "¿ A donde quieres ir?",
+    icon: "question",
+    html:
+      '<div class="swal_container">' +
+      '<a class="a_swal_2" href="https://github.com/educaTEam47/FrontEND" target="_blank">Git-Hub (FrontEnd)</a>' +
+      '<a class="a_swal_2" href="https://github.com/educaTEam47/BackENDGraphql" target="_blank">Git-Hub (BackEnd)</a>' +
       '</div>',
   });
 };
